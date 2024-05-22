8f35f3b1c38b2d88c43c43053e7ce6ac733b1f1c
TanHuynh68 <tanhpce171112@fpt.edu.vn>
2024-05-22 15:35:36 +0700

    fix header and footer

--- src/components/app.footer.tsx
+++ src/components/app.footer.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 const Footer = () => {
     return (
         <div className="bg-teal-500">
-            <footer className=" rounded-lg  dark:bg-gray-800">
+            <footer className="rounded-lg dark:bg-gray-800">
                 <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link href={"/"} >TAN'S RESUME</Link>
+                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link href={"/"} >TAN&apos;S RESUME</Link>
                     </span>
                     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                         <li>
@@ -26,9 +26,8 @@ const Footer = () => {
                     </ul>
                 </div>
             </footer>
-
         </div>
-    )
-}
+    );
+};
 
 export default Footer;
